2f15247cae6ae1e55d6ef48e23aa395b30b6610c
Brendan Bosman <bhbosman@gmail.com>
2022-07-27 00:59:23 +0200

    refactor

---
+++ internal/listener/ProvideConnectionReactor.go
@@ -0,0 +1,55 @@
+package listener
+
+import (
+	"github.com/bhbosman/goCommonMarketData/fullMarketDataHelper"
+	"github.com/bhbosman/goCommonMarketData/fullMarketDataManagerService"
+	"github.com/bhbosman/gocommon/GoFunctionCounter"
+	"github.com/bhbosman/gocommon/Services/interfaces"
+	"github.com/bhbosman/gocommon/model"
+	"github.com/bhbosman/gocommon/stream"
+	"github.com/bhbosman/gocomms/intf"
+	"github.com/bhbosman/goprotoextra"
+	"github.com/cskr/pubsub"
+	"go.uber.org/fx"
+	"go.uber.org/zap"
+	"golang.org/x/net/context"
+	"google.golang.org/protobuf/proto"
+)
+
+func ProvideConnectionReactor() fx.Option {
+	return fx.Options(
+		fx.Provide(
+			fx.Annotated{
+				Target: func(
+					params struct {
+						fx.In
+						CancelCtx              context.Context
+						CancelFunc             context.CancelFunc
+						ConnectionCancelFunc   model.ConnectionCancelFunc
+						Logger                 *zap.Logger
+						PubSub                 *pubsub.PubSub `name:"Application"`
+						UniqueReferenceService interfaces.IUniqueReferenceService
+						GoFunctionCounter      GoFunctionCounter.IService
+						FullMarketDataHelper   fullMarketDataHelper.IFullMarketDataHelper
+						FmdService             fullMarketDataManagerService.IFmdManagerService
+					},
+				) (intf.IConnectionReactor, error) {
+					return NewConnectionReactor(
+						params.Logger,
+						params.CancelCtx,
+						params.CancelFunc,
+						params.ConnectionCancelFunc,
+						params.PubSub,
+						func(data proto.Message) (goprotoextra.IReadWriterSize, error) {
+							return stream.Marshall(data)
+						},
+						params.GoFunctionCounter,
+						params.UniqueReferenceService,
+						params.FullMarketDataHelper,
+						params.FmdService,
+					)
+				},
+			},
+		),
+	)
+}
--- internal/listener/provide.go
+++ internal/listener/provide.go
@@ -13,19 +13,11 @@ import (
 	"github.com/bhbosman/goCommsStacks/protoBuf"
 	"github.com/bhbosman/goCommsStacks/topStack"
 	"github.com/bhbosman/gocommon/GoFunctionCounter"
-	"github.com/bhbosman/gocommon/Services/interfaces"
 	"github.com/bhbosman/gocommon/fx/PubSub"
 	"github.com/bhbosman/gocommon/messages"
-	"github.com/bhbosman/gocommon/model"
-	"github.com/bhbosman/gocommon/stream"
 	"github.com/bhbosman/gocomms/common"
-	"github.com/bhbosman/gocomms/intf"
-	"github.com/bhbosman/goprotoextra"
 	"github.com/cskr/pubsub"
 	"go.uber.org/fx"
-	"go.uber.org/zap"
-	"golang.org/x/net/context"
-	"google.golang.org/protobuf/proto"
 	"net/url"
 )
 
@@ -96,44 +88,6 @@ func CompressedListener(
 						),
 					)
 					return f(params.NetAppFuncInParams), nil
-				},
-			},
-		),
-	)
-}
-
-func ProvideConnectionReactor() fx.Option {
-	return fx.Options(
-		fx.Provide(
-			fx.Annotated{
-				Target: func(
-					params struct {
-						fx.In
-						CancelCtx              context.Context
-						CancelFunc             context.CancelFunc
-						ConnectionCancelFunc   model.ConnectionCancelFunc
-						Logger                 *zap.Logger
-						PubSub                 *pubsub.PubSub `name:"Application"`
-						UniqueReferenceService interfaces.IUniqueReferenceService
-						GoFunctionCounter      GoFunctionCounter.IService
-						FullMarketDataHelper   fullMarketDataHelper.IFullMarketDataHelper
-						FmdService             fullMarketDataManagerService.IFmdManagerService
-					},
-				) (intf.IConnectionReactor, error) {
-					return NewConnectionReactor(
-						params.Logger,
-						params.CancelCtx,
-						params.CancelFunc,
-						params.ConnectionCancelFunc,
-						params.PubSub,
-						func(data proto.Message) (goprotoextra.IReadWriterSize, error) {
-							return stream.Marshall(data)
-						},
-						params.GoFunctionCounter,
-						params.UniqueReferenceService,
-						params.FullMarketDataHelper,
-						params.FmdService,
-					)
 				},
 			},
 		),
